704bb7db19b77c7ac6dac54b7d0e24178173795a
Ruslan <54860448+heckfy88@users.noreply.github.com>
2025-12-29 15:46:59 +0300

    Добавлена реализация проекта

--- src/test/java/org/web/pages/HabrMainPage.java
+++ src/test/java/org/web/pages/HabrMainPage.java
@@ -3,7 +3,6 @@ package org.web.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HabrMainPage extends HabrBasePage {
@@ -14,7 +13,6 @@ public class HabrMainPage extends HabrBasePage {
 
     public HabrMainPage(WebDriver driver) {
         super(driver);
-        PageFactory.initElements(driver, this);
     }
 
     public void open() {
